0fb4581bd9022e411771d28d4ca6eed248223af5
Vincenzo <laveneziana.vincenzo@gmail.com>
2020-04-15 15:38:52 +0200

    Generazione numeri random

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
 console.log("OK andiamo");
+
+
+
+var casualNum = randomNumbers(1, 10)
+console.log(casualNum);
+//alert(casualNum);
+
+
+function randomNumbers(min , max) {
+  var output = [];
+  var appoggio = 0;
+  for (var i = 0; i < 5; i++) {
+
+  appoggio = Math.floor(Math.random() * max) + min ;
+
+  while(output.includes(appoggio)){
+  appoggio = Math.floor(Math.random() * max) + min ;
+  }
+
+  output.push(appoggio);
+  }
+
+  return output;
+}
+
+
