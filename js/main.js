b082bab164c698abd10dedde2a7bc3e2fd48b57a
Vincenzo <laveneziana.vincenzo@gmail.com>
2020-04-15 15:55:49 +0200

    fine logica js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,39 @@
 console.log("OK andiamo");
 
+var choise = parseInt(prompt("Inserisci un max range di numeri"))
 
+while( choise == 0 || choise <=5 || isNaN(choise)){
+  choise = parseInt(prompt("Inserisci un max range di numeri ccccc"))
+}
 
-var casualNum = randomNumbers(1, 10)
+var casualNum = randomNumbers(1, choise)
 console.log(casualNum);
-//alert(casualNum);
+alert("Cerca di ricordare i seguenti numeri " + casualNum);
+
+var numUtente = [];
+var numeriRicordati = [];
+
+setTimeout(function () {
+
+  for(var i = 0; i < 5; i++) {
+    numUtente.push(parseInt(prompt("Inserisci un numero")))
+  }
+  console.log(numUtente);
+
+  for( var o = 0; o < casualNum.length; o++){
+    if(casualNum.includes(numUtente[o])){
+      numeriRicordati.push(numUtente[o]);
+    }
+  }
+
+  if(numeriRicordati.length > 0){
+    console.log("i numeri ricordati sono: " + numeriRicordati.sort());
+  } else{
+    console.log("Non ti sei ricordato nessun numero");
+  }
+  
+},3000)
+
 
 
 function randomNumbers(min , max) {
